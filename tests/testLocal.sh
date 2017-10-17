9a48b871ca7e65dde5599bfed881cfa4744f1ffe
Kai Song <kaisong2@illinois.edu>
2017-10-17 00:42:43 -0500

    try to make travis work

--- tests/testLocal.sh
+++ tests/testLocal.sh
@@ -1,9 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
 CUR_DIR="$( cd "$( dirname "$0" )" && pwd )";
 
 testMvnOutputMin() {
-    result=`$CUR_DIR/../generate-basic-stats.sh -l $CUR_DIR/test-data/mvn_result_min.tx -d ./`
+    result=`$CUR_DIR/../generate-basic-stats.sh -l $CUR_DIR/test-data/mvn_result_min.txt -d ./`
     if [[ $? != 0 ]]; then
     	fail "There is an error running the testMvnOutputMin test"
     	return 1
