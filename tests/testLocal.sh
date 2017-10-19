41d91a01b5b37b1116857ec6b3247ab457eeb74f
Kai Song <kaisong2@illinois.edu>
2017-10-18 22:54:25 -0500

    added the test project as a github repo so that we can test travis. Added setUp and tearDown functions to get the repo live and delete it after test.

--- tests/testLocal.sh
+++ tests/testLocal.sh
@@ -20,15 +20,6 @@ testMvnOutputSecond() {
     assertTrue "[[ $result =~  5\.23 ]]"
 }
 
-testRunMvnLocally() {
-	result=`$CUR_DIR/../generate-basic-stats.sh -d $CUR_DIR/test-data/java-hello-proj`
-    if [[ $? != 0 ]]; then
-    	fail "There is an error running the testMvnOutputSecond test"
-    	return 1
-    fi
-    assertTrue "[[ $result =~ ^[-+]?[0-9]+(\.[0-9]+)?$ ]]"
-}
-
 testRunMvnTestFail() {
 	result=`$CUR_DIR/../generate-basic-stats.sh -d $CUR_DIR/test-data/`
 	assertEquals 1 $?
---
+++ tests/testRepo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+CUR_DIR="$( cd "$( dirname "$0" )" && pwd )";
+TEST_REPO_DIR="/tmp/java-hello-proj"
+
+setUp() {
+	if [ ! -d "$TEST_REPO_DIR" ]; then
+		cd /tmp/
+  		git clone git@github.com:ksong/java-hello-proj.git
+	fi
+}
+
+tearDown(){
+    rm -rf $TEST_REPO_DIR;
+}
+
+testRunMvnLocally() {
+	result=`$CUR_DIR/../generate-basic-stats.sh -d $TEST_REPO_DIR`
+    if [[ $? != 0 ]]; then
+    	fail "There is an error running the testMvnOutputSecond test"
+    	return 1
+    fi
+    assertTrue "[[ $result =~ ^[-+]?[0-9]+(\.[0-9]+)?$ ]]"
+}
+
+
+
+
+# Load shUnit2.
+
+. $CUR_DIR/../thirdparty/shunit2
